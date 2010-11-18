aafe87d215b9a68c2b32848b68a96503508df9ab
m13 <sergio.arcos@gmail.com>
2010-11-18 14:44:47 +0100

    first commit

---
+++ src/com/games/distetris.java
@@ -0,0 +1,13 @@
+package com.games;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class distetris extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
